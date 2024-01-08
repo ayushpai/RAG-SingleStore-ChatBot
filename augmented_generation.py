4f499c36caf4427e9f96fd37abea5c7c14f7632c
ayushpai <ayushpai@gmail.com>
2024-01-08 08:46:38 -0500

    Initial commit

---
+++ augmented_generation.py
@@ -0,0 +1,6 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import SingleStoreDB
+from langchain_openai import OpenAIEmbeddings
+
+
---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import openai
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores.singlestoredb import SingleStoreDB
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+os.environ["SINGLESTOREDB_URL"] = "admin:Test1234@svc-7301c603-3097-4c72-bc10-7881e89ff282-dml.aws-virginia-6.svc.singlestore.com:3306/RAGTester"
+
+# Load text samples
+loader = TextLoader("michael_jackson.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1500, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+
+docsearch = SingleStoreDB.from_documents(
+    docs,
+    embeddings,
+    table_name="notebook",  # use table with a custom name
+)
+
+query = "What does Michael Jackson have to do with Ronald Reagan?"
+docs = docsearch.similarity_search(query)  # Find documents that correspond to the query
+print(docs[0].page_content)
